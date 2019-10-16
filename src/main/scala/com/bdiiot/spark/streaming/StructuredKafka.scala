02837bb4b51d61e9a80ae53ce24873c51bbd0a20
Eber H <eber.h@outlook.com>
2019-10-16 18:58:16 +0800

    output to console success!

--- src/main/scala/com/bdiiot/spark/streaming/StructuredKafka.scala
+++ src/main/scala/com/bdiiot/spark/streaming/StructuredKafka.scala
@@ -7,12 +7,11 @@ import org.apache.spark.sql.streaming.Trigger
 
 object StructuredKafka {
   def main(args: Array[String]): Unit = {
-    if (args.length < 3) {
+    if (args.length < 2) {
       System.err.println(
         """
           |Usage: DirectKafkaWordCount <brokers> <topics>
           |  <brokers> is a list of one or more Kafka brokers
-          |  <groupId> is a group id
           |  <topics> is a list of one or more kafka topics to consume from
           |
         """.stripMargin)
@@ -22,7 +21,7 @@ object StructuredKafka {
     System.setProperty("java.security.krb5.conf", "/etc/krb5.conf")
     System.setProperty("java.security.auth.login.config", "/tmp/kafka_client_jaas.conf")
 
-    val Array(brokers, groupId, topics) = args
+    val Array(brokers, topics) = args
 
     val spark = SparkHelper.getSparkSession()
     import spark.implicits._
@@ -31,11 +30,6 @@ object StructuredKafka {
       .readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", brokers)
-
-      //Note that the following Kafka params cannot be set and the Kafka source or sink will throw an exception
-      //https://spark.apache.org/docs/latest/structured-streaming-kafka-integration.html#kafka-specific-configurations
-      .option("kafka.group.id", groupId)
-
       .option("subscribe", topics)
       .option("kafka.security.protocol", "SASL_PLAINTEXT")
       .option("startingOffsets", "earliest")
--- src/main/scala/com/bdiiot/spark/utils/JsonHelper.scala
+++ src/main/scala/com/bdiiot/spark/utils/JsonHelper.scala
@@ -5,38 +5,58 @@ import com.alibaba.fastjson.JSON
 import scala.collection.mutable.ListBuffer
 
 object JsonHelper {
+
+  def enableToJson(jsonStr: String): Boolean = {
+    var flag = false
+    try {
+      JSON.parseObject(jsonStr)
+      flag = true
+    } catch {
+      case ex: Exception => {
+        println(s"$ex")
+      }
+    }
+    flag
+  }
+
   def readJson(jsonStr: String): List[(String, String)] = {
-    val map = JSON.parseObject(jsonStr)
-
-    val database = {
-      val temp = map.getString("database")
-      if (temp == null) "none" else temp
-    }
-    val table = {
-      val temp = map.getString("table")
-      if (temp == null) "none" else temp
-    }
-    val sqlType = {
-      val temp = map.getString("type")
-      if (temp == null) "none" else temp
-    }
-
-    val key = new StringBuilder()
-      .append(database).append("-")
-      .append(table).append("-")
-      .append(sqlType).toString()
-
-    val dataArray = map.getJSONArray("data")
-    if (dataArray == null) {
-      return List.empty[(String, String)]
-    }
-    val it = dataArray.iterator()
     val valueList = new ListBuffer[(String, String)]()
-    while (it.hasNext) {
-      val eachRow = it.next().toString
-      valueList.append((key, eachRow))
+    if (enableToJson(jsonStr)) {
+      val map = JSON.parseObject(jsonStr)
+
+      val database = {
+        val temp = map.getString("database")
+        if (temp == null) "none" else temp
+      }
+      val table = {
+        val temp = map.getString("table")
+        if (temp == null) "none" else temp
+      }
+      val sqlType = {
+        val temp = map.getString("type")
+        if (temp == null) "none" else temp
+      }
+
+      val key = new StringBuilder()
+        .append(database).append("-")
+        .append(table).append("-")
+        .append(sqlType).toString()
+
+      val dataArray = map.getJSONArray("data")
+      if (dataArray == null) {
+        return List.empty[(String, String)]
+      }
+      val it = dataArray.iterator()
+
+      while (it.hasNext) {
+        val eachRow = it.next().toString
+        valueList.append((key, eachRow))
+      }
+      valueList.toList
+    } else {
+      valueList.append(("key", "eachRow"))
+      valueList.toList
     }
-    valueList.toList
   }
 
 }
